6dcc2d857edfa98b784f4abe6ae25bfe818d34fd
tohosgabi <tohosgabi@gmail.com>
2022-01-03 16:54:53 +0200

    implemented register functionality

--- app/src/main/java/com/piu/solar_energy_usage/MainWindowActivity.kt
+++ app/src/main/java/com/piu/solar_energy_usage/MainWindowActivity.kt
@@ -1,10 +1,11 @@
 package com.piu.solar_energy_usage
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.appbar.MaterialToolbar
 import com.piu.solar_energy_usage.car.CarActivity
@@ -23,12 +24,6 @@ class MainWindowActivity : AppCompatActivity() {
         }
     }
 
-    fun onLogOutButtonClicked(item: MenuItem) {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
-
     fun onWeatherButtonClicked(item: MenuItem) {
         val intent = Intent(this, WeatherActivity::class.java)
         startActivity(intent)
@@ -42,5 +37,29 @@ class MainWindowActivity : AppCompatActivity() {
     fun onCarButtonClicked(item: MenuItem) {
         val intent = Intent(this, CarActivity::class.java)
         startActivity(intent)
+    }
+
+    fun onLogOutButtonClicked(item: MenuItem) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("You are about to log out!")
+            .setMessage("Are you sure?")
+            .setPositiveButton("Logout") { _, _ ->
+                finish()
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+            .show()
+    }
+
+    override fun onBackPressed() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("You are about to log out!")
+            .setMessage("Are you sure?")
+            .setPositiveButton("Logout") { _, _ ->
+                finish()
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+            .show()
     }
 }
